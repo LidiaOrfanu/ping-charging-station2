599d843625390a00df1bf7886869d0a35fac5662
Lidia <80785509+LidiaOrfanu@users.noreply.github.com>
2023-09-13 10:43:49 +0200

    add and apply migrations

---
+++ server/Server.Api/Migrations/20230913084319_InitialCreateCS.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Server.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateCS : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ChargingStations",
+                columns: table => new
+                {
+                    StationId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    LocationId = table.Column<int>(type: "integer", nullable: true),
+                    isAvailable = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChargingStations", x => x.StationId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChargingStations");
+        }
+    }
+}
